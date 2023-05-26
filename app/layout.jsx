78667794c474f6aef0461b3dbfe63c6c9e53bb99
Jahid0178 <zahidul.haque767@gmail.com>
2023-05-27 01:29:05 +0600

    added metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,7 +12,10 @@ const ubuntu = Ubuntu({
 
 export const metadata = {
   title: "Zahidul Haque | Portfolio",
-  description: "This is zahidul haque portfolio.",
+  description:
+    "Welcome to my portfolio! I'm a skilled MERN Stack developer with a strong focus on front-end development. Explore my projects, showcasing my expertise in crafting responsive and user-friendly web applications. With a passion for creating seamless user experiences, I aim to bring innovative ideas to life using the latest technologies. Discover my skills in HTML, CSS, JavaScript, React, and more, and let's collaborate on your next digital endeavor.",
+  keywords:
+    "MERN Stack Developer, Front-End Developer, Web Application Development, Responsive Web Design, HTML5, CSS3, JavaScript, React.js, Portfolio Showcase, Innovative Web Solutions, Mobile-Friendly Development, Cross-Browser Compatibility, Performance Optimization, JavaScript Frameworks, Code Efficiency, Node Js, MongoDB, Next.Js Developer, Next.Js, Express.Js, React.js Developer",
 };
 
 export default function RootLayout({ children }) {
