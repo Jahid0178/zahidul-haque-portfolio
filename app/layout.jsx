37c0cb5bb28c5c8cb9b631f6c0f8344e8a794799
Jahid0178 <zahidul.haque767@gmail.com>
2023-05-27 01:30:10 +0600

    update again

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export const metadata = {
   description:
     "Welcome to my portfolio! I'm a skilled MERN Stack developer with a strong focus on front-end development. Explore my projects, showcasing my expertise in crafting responsive and user-friendly web applications. With a passion for creating seamless user experiences, I aim to bring innovative ideas to life using the latest technologies. Discover my skills in HTML, CSS, JavaScript, React, and more, and let's collaborate on your next digital endeavor.",
   keywords:
-    "MERN Stack Developer, Front-End Developer, Web Application Development, Responsive Web Design, HTML5, CSS3, JavaScript, React.js, Portfolio Showcase, Innovative Web Solutions, Mobile-Friendly Development, Cross-Browser Compatibility, Performance Optimization, JavaScript Frameworks, Code Efficiency, Node Js, MongoDB, Next.Js Developer, Next.Js, Express.Js, React.js Developer",
+    "MERN Stack Developer, Front-End Developer, Web Application Development, Responsive Web Design, HTML5, CSS3, JavaScript, React.js, Portfolio Showcase, Innovative Web Solutions, Mobile-Friendly Development, Cross-Browser Compatibility, Performance Optimization, JavaScript Frameworks, Code Efficiency, Node Js, MongoDB, Next.Js Developer, Next.Js, Express.Js, React.js Developer, React Js",
 };
 
 export default function RootLayout({ children }) {
