a95886ba7076159d9d19026a57b4cc6d2211743b
Jahid0178 <zahidul.haque767@gmail.com>
2023-05-26 22:50:42 +0600

    versel scrollToTop issue fixed

--- app/components/Buttons/ScrollToTop/ScrollToTop.js
+++ app/components/Buttons/ScrollToTop/ScrollToTop.js
@@ -1,28 +1,38 @@
-"use client";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 const ScrollToTop = () => {
   const [visible, setVisible] = useState(false);
 
   const scrollToTop = () => {
-    window.scrollTo({
-      behavior: "smooth",
-      top: 0,
-    });
-  };
-
-  const toggleVisible = () => {
-    const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 300) {
-      setVisible(true);
-    } else if (scrolled <= 300) {
-      setVisible(false);
+    if (typeof window !== "undefined") {
+      window.scrollTo({
+        behavior: "smooth",
+        top: 0,
+      });
     }
   };
 
-  window.addEventListener("scroll", toggleVisible);
+  const toggleVisible = () => {
+    if (typeof window !== "undefined") {
+      const scrolled = document.documentElement.scrollTop;
+      if (scrolled > 300) {
+        setVisible(true);
+      } else if (scrolled <= 300) {
+        setVisible(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", toggleVisible);
+
+      return () => {
+        window.removeEventListener("scroll", toggleVisible);
+      };
+    }
+  }, []);
 
   return (
     <button
