b8fac99866473445eb4e6267ece55547c80b7dc0
Jahid0178 <zahidul.haque767@gmail.com>
2023-06-10 05:50:21 +0600

    added blog page

---
+++ app/blogs/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+const BlogsPage = () => {
+  return (
+    <section>
+      <div className="container">
+        <h1 className="text-4xl text-center">Coming Soon 😉</h1>
+      </div>
+    </section>
+  );
+};
+
+export default BlogsPage;
